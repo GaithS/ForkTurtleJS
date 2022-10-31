6cfcfc747d130054f462261a36b3ec01684e1a96
GaithS <gshubailat.26@acsamman.edu.jo>
2022-10-31 09:30:42 +0200

    t

--- myscript.js
+++ myscript.js
@@ -13,6 +13,29 @@ function main()
     penup ();
     var min = -getWidth(100)/2;
     var max = getWidth(100)/2;
-   
-    
+    goto (-50,50)    
+}
+
+function main() 
+{
+    var sidelength = 150;
+
+    pendown();
+    var min = -getWidth()/2
+    var max = getWidth()/2
+    goto(-250,250);
+    console.log(turtle.pos);
+    console.log(sideLength);
+    square(sideLength);
+    left(45);
+    forward(705);
+    penup();
+    var min = -getWidth()/2
+    var max = getWidth()/2
+    goto(-250,250);
+    console.log(turtle.pos);
+    console.log(sideLength);
+    square(sideLength);
+    left(45);
+    forward(705);
 }
