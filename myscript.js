7eeab39f5d2d7071a9208a8457763e6990f8c99b
GaithS <gshubailat.26@acsamman.edu.jo>
2022-10-31 09:10:55 +0200

    g

--- myscript.js
+++ myscript.js
@@ -3,21 +3,15 @@ function main()
     console.log("Hello, world");
 }
 
-function square(side) {
-    for(var i = 0; i < 4; i++){
-       forward(side);
-       right(90);
-    }
- }
 
 function main()
 {
-    var sideLength = prompt("Side length:");
+    var x = prompt("x");
+    var y = prompt("y");
     
     pendown();
     var min = -getWidth()/2;
     var max = getWidth()/2;
-    goto(random(min, max), random(min, max))
-    square(sideLength);
+    penup (10)
     
 }
